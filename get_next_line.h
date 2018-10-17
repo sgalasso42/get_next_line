0c43409c2230cd2faa4092ea06e58ab72685ae89
Simon GALASSO <sgalasso@e3r11p8.42.fr>
2018-10-17 12:45:01 +0200

    get_next_line 42

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgalasso <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/08 14:16:14 by sgalasso          #+#    #+#             */
+/*   Updated: 2018/05/12 17:25:05 by sgalasso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 10
+# include "libft/libft.h"
+# include <sys/types.h>
+
+int		get_next_line(const int fd, char **line);
+
+#endif
